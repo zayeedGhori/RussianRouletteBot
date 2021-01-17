239412e19edc4e20c40afe73c786620c4bd81299
Zayeed Ghori <zmh030609@gmail.com>
2021-01-17 18:53:23 -0500

    Added test slash commands.

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import os
 import discord
 from dotenv import load_dotenv
+import requests
 
 # imports items from .env
 load_dotenv()
@@ -14,12 +15,47 @@ intents.members = True
 # Creates bot
 bot = discord.Client(intents=intents)
 
-# Initializes keyword that the bot will respond to
-keyword = '!roulette'
+url = "https://discord.com/api/v8/applications/<my_application_id>/commands"
 
-sub_commands = {
-
+json = {
+    "name": "blep",
+    "description": "Send a random adorable animal photo",
+    "options": [
+        {
+            "name": "animal",
+            "description": "The type of animal",
+            "type": 3,
+            "required": True,
+            "choices": [
+                {
+                    "name": "Dog",
+                    "value": "animal_dog"
+                },
+                {
+                    "name": "Cat",
+                    "value": "animal_cat"
+                },
+                {
+                    "name": "Penguin",
+                    "value": "animal_penguin"
+                }
+            ]
+        },
+        {
+            "name": "only_smol",
+            "description": "Whether to show only baby animals",
+            "type": 5,
+            "required": False
+        }
+    ]
 }
+
+# For authorization, you can use either your bot token 
+headers = {
+    "Authorization": "Bot 123456"
+}
+
+r = requests.post(url, headers=headers, json=json)
 
 # Checks if the GUILD in .env is listed is in the guilds of the bot and if it has connected to discord
 @bot.event
@@ -36,12 +72,7 @@ async def on_ready():
 # Runs when a message is sent, prints 'hello [member names]'
 @bot.event
 async def on_message(message):
-    message_text = message.content.strip().lower()
-    # prints message if keyword is in the start of message
-    print("message sent")
-    if message_text.find(keyword) == 0:
-        members = ', '.join([member.name for member in bot.guilds[0].members])
-        await message.channel.send("Hello! You have activated the Russian Roulette Bot.'{}'".format(members))
+    
 
 # runs the bot
 bot.run(TOKEN)
---
+++ hi.py
@@ -0,0 +1,6 @@
+import discord
+
+class Hi:
+    def __init__(self, client):
+        self.client = client
+
