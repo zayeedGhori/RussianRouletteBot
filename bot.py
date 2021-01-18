880c4b07c5e3e09a2ae52ebffbeea583bb41348e
Zayeed Ghori <zmh030609@gmail.com>
2021-01-17 19:47:52 -0500

    Added new command word using discord.ext. Added play command

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import os
 import discord
+from discord.ext import commands
 from dotenv import load_dotenv
 import requests
 
@@ -13,49 +14,49 @@ intents = discord.Intents.default()
 intents.members = True
 
 # Creates bot
-bot = discord.Client(intents=intents)
+bot = commands.Bot("!", help_command=)
 
-url = "https://discord.com/api/v8/applications/787821652364361748/commands"
+# url = "https://discord.com/api/v8/applications/787821652364361748/commands"
 
-json = {
-    "name": "blep",
-    "description": "Send a random adorable animal photo",
-    "options": [
-        {
-            "name": "animal",
-            "description": "The type of animal",
-            "type": 3,
-            "required": True,
-            "choices": [
-                {
-                    "name": "Dog",
-                    "value": "animal_dog"
-                },
-                {
-                    "name": "Cat",
-                    "value": "animal_cat"
-                },
-                {
-                    "name": "Penguin",
-                    "value": "animal_penguin"
-                }
-            ]
-        },
-        {
-            "name": "only_smol",
-            "description": "Whether to show only baby animals",
-            "type": 5,
-            "required": False
-        }
-    ]
-}
+# json = {
+#     "name": "blep",
+#     "description": "Send a random adorable animal photo",
+#     "options": [
+#         {
+#             "name": "animal",
+#             "description": "The type of animal",
+#             "type": 3,
+#             "required": True,
+#             "choices": [
+#                 {
+#                     "name": "Dog",
+#                     "value": "animal_dog"
+#                 },
+#                 {
+#                     "name": "Cat",
+#                     "value": "animal_cat"
+#                 },
+#                 {
+#                     "name": "Penguin",
+#                     "value": "animal_penguin"
+#                 }
+#             ]
+#         },
+#         {
+#             "name": "only_smol",
+#             "description": "Whether to show only baby animals",
+#             "type": 5,
+#             "required": False
+#         }
+#     ]
+# }
 
-# For authorization, you can use either your bot token 
-headers = {
-    "Authorization": TOKEN
-}
+# # For authorization, you can use either your bot token 
+# headers = {
+#     "Authorization": TOKEN
+# }
 
-r = requests.post(url, headers=headers, json=json)
+# r = requests.post(url, headers=headers, json=json)
 
 # Checks if the GUILD in .env is listed is in the guilds of the bot and if it has connected to discord
 @bot.event
@@ -69,16 +70,10 @@ async def on_ready():
         f'{bot.user} has connected to Discord!\n'
     )
 
-# # Runs when a message is sent, prints 'hello [member names]'
-# @bot.event
-# async def on_message(message):
-#     message_breakdown = message.content.strip().lower().split()
-#     main, extra = message_breakdown[0], message_breakdown[1:len(message_breakdown)]
-#     # prints message if keyword is in the start of message
-#     if main == keyword:
-#         extra = (" / ").join(extra)
-#         members = (', ').join([member.name for member in bot.guilds[0].members])
-#         await message.channel.send("Hello! You have activated the Russian Roulette Bot. You have added the following sub-commands: [{}]. Current members are: '{}'".format(extra, members))
+@bot.command()
+async def play(ctx, players: ):
+    await ctx.send("You are playing!")
+
     
 
 # runs the bot
