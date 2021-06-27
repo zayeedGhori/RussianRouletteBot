9a57ea17ee1e2d02a1897710847b224d588ef3db
Abii-23 <abishanan.s@gmail.com>
2021-06-27 15:12:53 -0400

    Fixed embeds in gameplay messages.

--- cogs/play.py
+++ cogs/play.py
@@ -5,6 +5,16 @@ from discord.ext import commands
 FILLER = "[FILLER]"
 NUMBER_OF_ROUNDS = 6
 REQUIRED_NUM_PLAYERS = 2
+CONGRATS_MESSAGE = "Congratulations! You earned {} coins! 💰"
+NEXT_PLAYER_MESSAGES = {
+    "You're up",
+    "Next up",
+    "You got this",
+    "Don't blow it",
+    "Let's go",
+    "Come on",
+    "Take a chance"
+}
 FUNNY_MESSAGES = {
     "Survived": [
         "Lucky you. 🍀",
@@ -22,11 +32,11 @@ FUNNY_MESSAGES = {
 TIME_TO_RESPOND = 60
 
 def createEmbed(title="", desc="test", fields=[], colour=discord.Color.dark_blue()):
-    title, desc = str(title), str
+    title, desc = str(title), str(desc)
     embed = discord.Embed(title = title, description = desc, colour = colour)
 
     for field in fields:
-        embed.add_field(name = field["name"], value = field["value"], inline = field["inline"])
+        embed.add_field(name = ("name" in field.keys()) and field["name"] or "", value = field["value"], inline = field["inline"])
 
     return embed
 
@@ -57,11 +67,11 @@ class Play(commands.Cog):
             if not self.user_is_in_game(member):
                 members.append(member)
             else:
-                await ctx.send(embed=createEmbed(title = "Can't join!", desc=f'User {member.display_name} is currently in a game! They have been omitted.', colour = discord.Color.dark_gray()))
+                await ctx.send(embed=createEmbed(title = "Can't join!", desc="User {} is currently in a game! They have been omitted.".format(member.display_name), colour = discord.Color.dark_gray()))
 
         # Create revolver with bullet (the location being the index of the True value)
         if len(members) < REQUIRED_NUM_PLAYERS:
-            await ctx.send(embed=createEmbed(title = "Not enough players!", desc=f'You do not have enough players! {REQUIRED_NUM_PLAYERS-len(members)} more players needed.', colour = discord.Color.red())) 
+            await ctx.send(embed=createEmbed(title = "Not enough players!", desc="You do not have enough players! {} more players needed.".format(REQUIRED_NUM_PLAYERS-len(members)), colour = discord.Color.red())) 
             return
 
         self.GAMES_PLAYED += 1
@@ -76,19 +86,24 @@ class Play(commands.Cog):
         cylinder = [True for _ in range(NUMBER_OF_ROUNDS)]
         cylinder[random.randint(0, NUMBER_OF_ROUNDS-1)] = False
         # Let members know what mode they are using
-        await ctx.send(embed=createEmbed(desc=f'{", ".join(member.display_name for member in members)} are playing regular mode!'))
+        await ctx.send(embed=createEmbed(desc="{} are playing regular mode!".format(", ".join(member.display_name for member in members))))
 
         ### Short game of automated imperfect russian roulette ###
         Round = 0
         while len(members) > 1:
             # Increment and display round
             Round += 1 
-            await ctx.send(embed=createEmbed(title=f'Round {Round}:', desc=""))
+            roundEmbed = createEmbed(title="Round {}:".format(Round), desc="")
 
+            isFirst = True
             # for every member playing, make them shoot
             for member in members:
-
-                await ctx.send(embed=createEmbed(desc=f'Your turn {member.display_name}!'))
+                if isFirst:
+                    isFirst = False
+                    roundEmbed.add_field(name = "{}...".format(NEXT_PLAYER_MESSAGES[random.randint(1, len(NEXT_PLAYER_MESSAGES))-1]), value="Your turn {}!".format(member.display_name), inline=False)
+                    await ctx.send(embed=roundEmbed)
+                else:
+                    await ctx.send(embed=createEmbed(desc="Your turn {}!".format(member.display_name)))
                 print(self.Games[game_id]["CurrentPlayer"])
                 self.Games[game_id]["CurrentPlayer"] = member
 
@@ -121,30 +136,31 @@ class Play(commands.Cog):
                 try:
                     await self.bot.wait_for("message", check=verify, timeout=60.0)
                 except asyncio.TimeoutError:
-                    
-                    await ctx.send(embed=createEmbed(desc=f'{member.display_name} failed to respond in time!'))
+                    await ctx.send(embed=createEmbed(desc="{} failed to respond in time!".format(member.display_name)))
                     members.remove(member) 
                 else:
-                    shotEmbed = createEmbed(desc=f'{member.display_name} shot, and ...')
+                    shotEmbed = createEmbed(desc="{} shot, and ...".format(member.display_name))
                     
                     # If safe, then no bullet+
                     safe = cylinder.pop(random.randint(0, len(cylinder)-1))
-                    if safe:   
-                        shotEmbed = createEmbed(desc=f'{member.display_name} shot, and ...', colour = discord.Color.green())
+                    if safe:                           
                         safe_message = FUNNY_MESSAGES["Survived"][random.randint(0, len(FUNNY_MESSAGES["Survived"])-1)]
-                        shotEmbed.add_field(name="safe", value=f'is safe! {safe_message}', inline=False)
+
+                        shotEmbed.colour = discord.Colour.green()
+                        shotEmbed.add_field(name = "is safe!", value=safe_message, inline=False)
                     
                     # if not safe, member is dead
                     else:
-                        shotEmbed = createEmbed(desc=f'{member.display_name} shot, and ...')
                         died_message = FUNNY_MESSAGES["Died"][random.randint(0, len(FUNNY_MESSAGES["Died"])-1)]
-                        shotEmbed.add_field(name="safe", value=f'is dead! {died_message}', inline=False)
+
+                        shotEmbed.colour = discord.Colour.red()
+                        shotEmbed.add_field(name = "is dead!", value=died_message, inline=False)
                         members.remove(member) # remove member from members list
 
                     await ctx.send(embed = shotEmbed)
 
         # Win message
-        await ctx.send(embed=createEmbed(title=f'{members[0].mention} wins!', colour=discord.Color.gold()))
+        await ctx.send(embed=createEmbed(title="{} wins!".format(members[0].display_name), desc = CONGRATS_MESSAGE, colour=discord.Color.gold()))
         
         self.Games.pop(game_id, None)
         # award points here
